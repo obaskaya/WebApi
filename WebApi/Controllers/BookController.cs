6e51c9cd26f1777710a61abad7da406f11e2ee63
oguzhan <o.baskaya@yahoo.com>
2023-02-04 00:45:51 +0300

    Add project files.

---
+++ WebApi/Controllers/BookController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApi.BookOperations;
+using WebApi.Data;
+using WebApi.DBOperations;
+using static WebApi.BookOperations.CreateBookCommand;
+
+namespace WebApi.Controllers
+{
+	[ApiController]
+	[Route("[controller]s")]
+	public class BookController : ControllerBase
+	{
+		private readonly BookStoreDbContext _context;
+		public BookController(BookStoreDbContext context)
+		{
+			_context = context;
+		}
+
+
+		[HttpGet]
+		public IActionResult GetBooks()
+		{
+			GetBooksQuery query = new GetBooksQuery(_context);
+			var result = query.Handle();
+			return Ok(result);
+
+		}
+
+		// get by id
+		[HttpGet("{id}")]
+
+		public Book GetById(int id)
+		{
+			var book = _context.Books.Where(book => book.Id == id).SingleOrDefault();
+			return book;
+
+		}
+
+		// add book 
+		[HttpPost]
+		public IActionResult AddBook([FromBody] CreateBookModel newBook)
+		{
+			CreateBookCommand command = new CreateBookCommand(_context);
+			try
+			{
+				command.Model = newBook;
+				command.Handle();
+
+			}
+			catch (Exception ex)
+			{
+
+				return BadRequest(ex.Message);
+			}
+			return Ok();
+
+			
+		}
+
+		// update book
+		[HttpPut("{id}")]
+		public IActionResult UpdateBook(int id, [FromBody] Book updatedBook)
+		{
+			var book = _context.Books.SingleOrDefault(x => x.Id == id);
+			if (book is null)
+			{
+				return BadRequest();
+			}
+			book.GenreId = updatedBook.GenreId != default ? updatedBook.GenreId : book.GenreId;
+			book.PageCount = updatedBook.PageCount != default ? updatedBook.PageCount : book.PageCount;
+			book.PublishDate = updatedBook.PublishDate != default ? updatedBook.PublishDate : book.PublishDate;
+			book.Title = updatedBook.Title != default ? updatedBook.Title : book.Title;
+
+			_context.SaveChanges();
+			return Ok();
+		}
+		[HttpDelete("{id}")]
+		public IActionResult DeleteBook(int id)
+		{
+			var book = _context.Books.SingleOrDefault(x => x.Id == id);
+			if (book is null)
+			{
+				return BadRequest();
+			}
+
+			_context.Books.Remove(book);
+			_context.SaveChanges();
+
+			return Ok();
+		}
+	}
+}
---
+++ WebApi/DBOperations/DataGenerator.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using WebApi.Data;
+
+
+namespace WebApi.DBOperations
+{
+	public class DataGenerator
+	{
+		public static void Initialize(IServiceProvider serviceProvider)
+		{
+			using (var context = new BookStoreDbContext(
+			serviceProvider.GetRequiredService<DbContextOptions<BookStoreDbContext>>()))
+			
+			{
+				// Look for any book.
+				if (context.Books.Any())
+				{
+					return;   // Data was already seeded
+				}
+				context.Books.AddRange(
+					new Book
+					{
+						//Id = 1,
+						Title = "Lean Startup",
+						GenreId = 1,
+						PageCount = 200,
+						PublishDate = new DateTime(2012, 09, 02)
+					},
+					new Book
+					{
+						//Id = 2,
+						Title = "FbSpor",
+						GenreId = 2,
+						PageCount = 1907,
+						PublishDate = new DateTime(1907, 07, 03)
+					}
+				);
+				context.SaveChanges();
+			}
+
+		}
+	}
+}
